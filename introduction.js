f4c5bae68d884dfa5e325afb9dd14a9e42bf7395
mac <mac@macs-MacBook-Pro.local>
2024-10-30 00:54:35 +0700

    first commit

---
+++ introduction.js
@@ -0,0 +1,35 @@
+// My First JS Code!
+/* 
+
+
+ini adalah program
+
+
+
+*/
+// console.talk();
+console.log("Hello World");
+console.log(5000*1000);
+
+const alamat = "Arya Suhara";
+console.log(alamat);
+
+/* ways to create */
+// Global Scope
+// Console.log(age1);
+// Console.log(age2);
+//consle.log(age3);
+
+{
+    //Lv. 1 Scope
+    {
+        //Lv. 2 Scope
+        var age1 = 10;
+        let age2 = 20;
+        const age3 = 30;
+        {
+            //Lv. 3 Scope
+            // console.log(age3);
+        }
+    }
+}
